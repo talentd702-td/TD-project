cb9c38874faf1c659c17c88e88c78c47f56cd196
Surya-blippi <suryansh.connect@gmail.com>
2025-11-12 18:40:11 +0530

    Updated Code

--- src/pages/landing/index.js
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-// Import from components folder now
-const LandingPage = dynamic(
-  () => import("../../src/pages/landing/LandingPage"), 
-  { ssr: false }
-);
-
-export default LandingPage;
